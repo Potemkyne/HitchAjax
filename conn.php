628d0830dc2997ebef0112a83b445234f852c643
Potemkyne <potemkyne.github@gmail.com>
2017-08-05 17:42:48 +0200

    modif bdd

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "hitchcock";
+$dbname = "rebecca";
 try {
 	$conn = new PDO( "mysql:host=$servername; dbname=$dbname", $username, $password , array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8') );
 	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
--- formextractall.php
+++ formextractall.php
@@ -2,7 +2,7 @@
 
 include( "conn.php" );
 try {
-	$sql = " select titre_fr,titre_en,annee,synopsis,nationalite,duree  from film ";
+	$sql = " select frtitle from movie ";
 	$stmt = $conn->prepare( $sql );
 	$stmt->execute();
 	$response = $stmt->fetchall(PDO::FETCH_ASSOC);
